99e5f4e8856fb3d2350eda437065ff979e759538
VasilevAnatoly <tolya051996@mail.ru>
2019-03-15 15:21:49 +0300

    Add cors to server

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const initDB = require('./init/index');
 initDB.init();
 
 const koa = require('koa');
+const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const router = require('./app/router');
 const app = new koa();
 
+app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
